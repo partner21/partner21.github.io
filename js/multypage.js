df8eb80b6b32daf014280c4805e0a1fc069912e3
Fakeminer <87234992+Fakeminer@users.noreply.github.com>
2022-02-01 17:02:41 +0400

    Add files via upload

--- js/multypage.js
+++ js/multypage.js
@@ -1,16 +1,19 @@
-// Ищем все блоки, имя которых оканчивается на -page
-pages = document.querySelectorAll('[class$="-page"]');
+// Берем query-поле  "page"
+let page = getQueryVariable('page')
+
+// Ищем все блоки, с дата-аттрибутом page
+pages = document.querySelectorAll('[data-page]');
 // Скрываем все такие блоки
 for(let i = 0; i < pages.length; i++) {
     pages[i].style.display = 'none';
 }
 // Проверяем, не пустое ли поле page в ссылке на страницу
 if(getQueryVariable('page') != false) {
-    // Проверяем, если ли класс с названием этой переменной + -page
-    if(document.querySelector('.'+getQueryVariable('page')+'-page')!=null) {
-        // Получаем класс с названием этой переменной + -page
+    // Проверяем, если ли дата-атрибут page равный нашему полю
+    if(document.querySelector('[data-page='+ page +']')!=null) {
+        // Получаем нужный нам дата-аттрибут
         document
-            .querySelector('.'+getQueryVariable('page')+'-page')
+            .querySelector('[data-page=' + page + ']')
             // Обращаемся к аттрибуту style этого тега
             .style
             // Устанавливаем display в значение block
